8208197f7bcd767209cedd19509bce4efd69ce74
JamesPeekie <33199812+JamesPeekie@users.noreply.github.com>
2017-11-07 09:37:41 +0000

    Update TurretManager.cs

--- TowerDefense/Assets/Scripts/TurretManager.cs
+++ TowerDefense/Assets/Scripts/TurretManager.cs
@@ -5,80 +5,80 @@ using UnityEngine.UI;
 
 public class TurretManager : MonoBehaviour
 {
-    [SerializeField] private GameObject placePath;
-    [SerializeField] private Text inUseText;
-    [SerializeField] private Text brokeText;
-    [SerializeField] private Text removeCostText;
+	[SerializeField] private GameObject placePath;
+	[SerializeField] private Text inUseText;
+	[SerializeField] private Text brokeText;
+	[SerializeField] private Text removeCostText;
 
-    public static TurretManager singleton;
-    private TurretStats turretToBuild;
-
-    void Awake ()
+	public static TurretManager singleton;
+	private TurretStats turretToBuild;
+	
+	void Awake ()
 	{
 		if (singleton != null)
-        {
+		{
 			Debug.LogError("Multiple TurretManagers");
 			return; 
 		}
 
 		singleton = this; 
 	}
-
-    void Start()
-    {
-        placePath.SetActive(false);
-        inUseText.gameObject.SetActive(false);
-        brokeText.gameObject.SetActive(false);
-        removeCostText.gameObject.SetActive(false);
-    }
-
-    public void ShowInUseMessage() // Is called when a node is already occupied by a turret
-    {
-        inUseText.gameObject.SetActive(true); // Activates text telling the player the turret space is in use
-        inUseText.gameObject.GetComponent<Animation>().Play(); // Plays the animation for the text activated
-        HidePlacePath();
-    }
-
-    public void BuildTurretOn(Node node) 
+	
+	void Start()
 	{
-        removeCostText.text = string.Format("-${0}", Mathf.RoundToInt(turretToBuild.cost).ToString()); // Sets text showing the player how much money has been removed
+		placePath.SetActive(false);
+		inUseText.gameObject.SetActive(false);
+		brokeText.gameObject.SetActive(false);
+		removeCostText.gameObject.SetActive(false);
+	}
 
-        if (PlayerManager.money < turretToBuild.cost) // Checks if you have no money left.
-        {
-            brokeText.gameObject.SetActive(true);
-            brokeText.gameObject.GetComponent<Animation>().Play();
-            HidePlacePath();
-            return;
-        }
+	public void ShowInUseMessage() // Is called when a node is already occupied by a turret
+	{
+		inUseText.gameObject.SetActive(true); // Activates text telling the player the turret space is in use
+		inUseText.gameObject.GetComponent<Animation>().Play(); // Plays the animation for the text activated
+		HidePlacePath();
+	}
 
-        PlayerManager.money -= turretToBuild.cost;
-        removeCostText.gameObject.SetActive(true);
-        removeCostText.gameObject.GetComponent<Animation>().Play();
-        GameObject turret = Instantiate(turretToBuild.turretPrefab, node.GetBuildPosition(), Quaternion.identity);
+	public void BuildTurretOn(Node node) 
+	{
+		removeCostText.text = string.Format("-${0}", Mathf.RoundToInt(turretToBuild.cost).ToString()); // Sets text showing the player how much money has been removed
+
+		if (PlayerManager.money < turretToBuild.cost) // Checks if you have no money left.
+		{
+			brokeText.gameObject.SetActive(true);
+			brokeText.gameObject.GetComponent<Animation>().Play();
+			HidePlacePath();
+			return;
+		}
+		
+		PlayerManager.money -= turretToBuild.cost;
+		removeCostText.gameObject.SetActive(true);
+		removeCostText.gameObject.GetComponent<Animation>().Play();
+		GameObject turret = Instantiate(turretToBuild.turretPrefab, node.GetBuildPosition(), Quaternion.identity);
 		node.turret = turret;
-        HidePlacePath();
+		HidePlacePath();
 	}
 
 	public bool CanBuild // Variable set to result of whether the turret is not empty
-    {
-        get
-        {
-            return turretToBuild != null;
-        }
-    }
+	{
+		get
+		{
+			return turretToBuild != null;
+		}
+	}
 
-    public bool HasMoney
-    {
-        get
-        {
-            return PlayerManager.money >= turretToBuild.cost;
-        }
-    }
+	public bool HasMoney
+	{
+		get
+		{
+			return PlayerManager.money >= turretToBuild.cost;
+		}
+	}
 
 	public void SelectTurretToBuild(TurretStats turret) // Vets the turret currently defined by this script as the listing for the stats script
 	{
-        turretToBuild = turret;
-        placePath.SetActive(true);
+		turretToBuild = turret;
+		placePath.SetActive(true);
 	}
 
     public void HidePlacePath()
